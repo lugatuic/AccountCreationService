ab40fc9bf86d9ddbe8e9f6d53d10f3fed2c751a4
Brent Yurek <brentagainagain@gmail.com>
2019-08-15 14:49:47 -0500

    Add secret keys and route to signup form

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import mysql.connector
 
 from signupform import SignupForm
@@ -11,3 +11,13 @@ db = mysql.connector.connect(
     passwd="password",
     database="LUGDB"
 )
+
+app.config.update(dict(
+    SECRET_KEY="I don't see why this is necessary",
+    WTF_CSRF_SECRET_KEY="I mean I don't see how this is benefitting in any way"
+))
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    return render_template('signup.html', title='Sign Up', form=form)
