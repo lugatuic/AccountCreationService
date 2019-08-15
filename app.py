d8e1495331876d2ffbbb8d7629ba99611b30424d
Brent Yurek <brentagainagain@gmail.com>
2019-08-15 16:08:38 -0500

    Send form data to database

--- app.py
+++ app.py
@@ -17,7 +17,15 @@ app.config.update(dict(
     WTF_CSRF_SECRET_KEY="I mean I don't see how this is benefitting in any way"
 ))
 
+memberSQL = "INSERT INTO Member (lastName, fullLegalName, preferredName, netid, email, UIN, major) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+lugdataSQL = "INSERT INTO LUGDATA (UIN, lugUserName, password) VALUES (%s, %s, %s)"
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     form = SignupForm()
+    if form.validate_on_submit():
+        cur = db.cursor()
+        cur.execute(memberSQL, (form.lastname, form.firstname, form.prefname, form.netid, form.email, form.uin, form.major))
+        cur.execute(lugdataSQL, (form.uin, form.username, form.password))
+        cur.commit()
     return render_template('signup.html', title='Sign Up', form=form)
