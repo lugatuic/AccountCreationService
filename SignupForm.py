72c766398ade5ec65f2066c4421936b4145570e5
Brent Yurek <brentagainagain@gmail.com>
2019-08-15 14:03:55 -0500

    Make sure confirm password field matches password

--- SignupForm.py
+++ SignupForm.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, IntegerField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 class SignupForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirmpass = PasswordField('Confirm Password', validators=[DataRequired()])
+    confirmpass = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo(password)])
     firstname = StringField('First Name', validators=[DataRequired()])
     lastname = StringField('Last Name', validators=[DataRequired()])
     prefname = StringField('Preferred Name')
