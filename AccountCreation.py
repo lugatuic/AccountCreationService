09d3bb748a4fdaa180d4ebbc5a9530bbf12fe9ec
Brent Yurek <brentagainagain@gmail.com>
2019-08-15 13:22:36 -0500

    Move SIgnupForm to separate file.

--- AccountCreation.py
+++ AccountCreation.py
@@ -1,9 +1,7 @@
 from flask import Flask
 import mysql.connector
 
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, IntegerField, SubmitField
-from wtforms.validators import DataRequired, Email, Length
+from SignupForm import SignupForm
 
 app = Flask(__name__)
 
@@ -13,16 +11,3 @@ db = mysql.connector.connect(
     passwd="password",
     database="LUGDB"
 )
-
-class SignupForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirmpass = PasswordField('Confirm Password', validators=[DataRequired()])
-    firstname = StringField('First Name', validators=[DataRequired()])
-    lastname = StringField('Last Name', validators=[DataRequired()])
-    prefname = StringField('Preferred Name')
-    netid = StringField('netID', validators=[DataRequired()])
-    email = StringField('email', validators=[DataRequired(), Email()])
-    uin = IntegerField('UIN', validators=[DataRequired()])
-    major = StringField('Major', validators=[DataRequired()])
-    submit = SubmitField('Sign Up')
---
+++ SignupForm.py
@@ -0,0 +1,16 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, IntegerField, SubmitField
+from wtforms.validators import DataRequired, Email, Length
+
+class SignupForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirmpass = PasswordField('Confirm Password', validators=[DataRequired()])
+    firstname = StringField('First Name', validators=[DataRequired()])
+    lastname = StringField('Last Name', validators=[DataRequired()])
+    prefname = StringField('Preferred Name')
+    netid = StringField('netID', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    uin = IntegerField('UIN', validators=[DataRequired()])
+    major = StringField('Major', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
