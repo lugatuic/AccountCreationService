afab1d5156f9c4b59e1feae9b6b1a7317cbfb60d
Brent Yurek <brentagainagain@gmail.com>
2019-08-17 11:04:49 -0500

    Designate a regular expression string with r

--- signupform.py
+++ signupform.py
@@ -9,7 +9,7 @@ class SignupForm(FlaskForm):
     firstname = StringField('First Name', validators=[DataRequired()])
     lastname = StringField('Last Name', validators=[DataRequired()])
     prefname = StringField('Preferred Name')
-    netid = StringField('netID', validators=[DataRequired(), Regexp('\w{2,6}\d{1,3}', message='Not a valid netID.')])
+    netid = StringField('netID', validators=[DataRequired(), Regexp(r'\w{2,6}\d{1,3}', message='Not a valid netID.')])
     email = StringField('email', validators=[DataRequired(), Email()])
     uin = IntegerField('UIN', validators=[DataRequired(), Length(9,9,message='A UIN is 9 digits long.')])
     major = SelectField('Major', choices=[('cs', 'Computer Science'), ('ce', 'Computer Engineering')], validators=[DataRequired()])
